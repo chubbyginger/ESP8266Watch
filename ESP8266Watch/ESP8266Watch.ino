d9b52f174285c072c5388861d0bc2effb85181c4
yangshunhuai <67225026+yangshunhuai@users.noreply.github.com>
2021-08-16 17:09:29 +0800

    ESP8266Watch: Remove connection

--- ESP8266Watch/ESP8266Watch.ino
+++ ESP8266Watch/ESP8266Watch.ino
@@ -1,28 +1,11 @@
 /*
    ESP8266Watch
-   - Connection -
-   DS1302:
-   CLK => GPIO 14 (D5)
-   DAT => GPIO 12 (D6)
-   RST => GPIO 13 (D7)
-
-   SSD1306 0.96'' OLED:
-   SDA => GPIO 4 (D2)
-   SCL => GPIO 5 (D1)
-
-   Potentiometer:
-   Connect the two ends to VCC and GND, and the pin in the middle to ADC0 (A0).
-
-   Touch button:
-   SIG => GPIO 15 (D8)
+   Connection as the schematics in the hardware folder.
 */
 
-// Include necessary libraries.
-#include <Arduino.h>
 #include <DS1302.h>
 #include <Wire.h>
 #include <U8g2lib.h>
-//#include <U8g2wqy.h>
 #include <OneButton.h>
 
 // You can change the pins here.
@@ -30,26 +13,17 @@
 #define POTPIN A0
 // The button pin:
 #define BTNPIN 15
-// DS1302 CLK, DAT and RST pins:
+// DS1302 pins:
 #define CLK 14
 #define DAT 12
 #define RST 13
-// The screen can only be connected to the hardware I2C bus,
-// but you can edit the screen width and height here, if you
-// are wanting to use another size.
-//
 // If your screen has a reset pin, change the reset pin below.
 // If your screen doesn't have a reset pin, leave it -1.
-//
-// If you want another I2C bus address, change it below.
-// Mine is using 0x3C. A generic 12864 should be this.
-// I am using a 0.96 inch 12864 one here.
 #define SCR_WIDTH 128
 #define SCR_HEIGHT 64
 #define SCR_RESET -1
-#define SCR_ADDR 0x3C
 
-// Some definitions to make code cleaner.
+// Some useful definitions
 #define ALIGN_CENTER(a) ((SCR_WIDTH - (u8g2.getUTF8Width(a))) / 2)
 #define ALIGN_RIGHT(a) (SCR_WIDTH -  u8g2.getUTF8Width(a))
 #define ALIGN_LEFT 0
